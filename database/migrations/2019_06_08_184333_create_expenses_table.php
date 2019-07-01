650be69609f6c155d2c62dc504ffe0bb8c167e61
Elizeu <elizeubragasantos@gmail.com>
2019-07-01 18:47:16 -0300

    tabela produtos para expenses

--- database/migrations/2019_06_08_184333_create_expenses_table.php
+++ database/migrations/2019_06_08_184333_create_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->double('price');
@@ -33,6 +33,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('expenses');
     }
 }
