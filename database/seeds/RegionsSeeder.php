0cddba14cdae8f5eb680e7b425ff318b3c0be91f
Elizeu <elizeubragasantos@gmail.com>
2019-07-20 15:13:05 -0300

    Issue seeder regions

--- database/seeds/RegionsSeeder.php
+++ database/seeds/RegionsSeeder.php
@@ -13,42 +13,42 @@ class RegionsSeeder extends Seeder
     {
         //
         DB::table('regions')->insert([
-            'name' => 'Vivendas Bela Vista',
+            'name' => 'Vivendas Bela Vista 1',
             'price' => 0,
             'user_id' => 1,
             'rate_id' => 3
         ]);
 
         DB::table('regions')->insert([
-            'name' => 'Sobradinho 1',
+            'name' => 'Sobradinho 1 1',
             'price' => 0,
             'user_id' => 1,
             'rate_id' => 1
         ]);
 
         DB::table('regions')->insert([
-            'name' => 'Sobradinho 2',
+            'name' => 'Sobradinho 2 1',
             'price' => 0,
             'user_id' => 1,
             'rate_id' => 2
         ]);
 
         DB::table('regions')->insert([
-            'name' => 'Vivendas Bela Vista',
+            'name' => 'Vivendas Bela Vista 2',
             'price' => 0,
             'user_id' => 2,
             'rate_id' => 3
         ]);
 
         DB::table('regions')->insert([
-            'name' => 'Sobradinho 1',
+            'name' => 'Sobradinho 1 2',
             'price' => 0,
             'user_id' => 2,
             'rate_id' => 1
         ]);
 
         DB::table('regions')->insert([
-            'name' => 'Sobradinho 2',
+            'name' => 'Sobradinho 2 2',
             'price' => 0,
             'user_id' => 2,
             'rate_id' => 2
