67dc0ab6f1a6f512d9984cff8bb7fc73602ecfc5
Elizeu <elizeubragasantos@gmail.com>
2019-07-20 14:27:48 -0300

    now() na FreelancesSeeder

--- database/seeds/FreelancesSeeder.php
+++ database/seeds/FreelancesSeeder.php
@@ -15,35 +15,41 @@ class FreelancesSeeder extends Seeder
         DB::table('freelances')->insert([
             'obs' => 'L 2',
             'user_id' => 1,
-            'region_id' => 1 //Vivendas
+            'region_id' => 1, //Vivendas
+            'created_at' => now(),
         ]);
 
         DB::table('freelances')->insert([
             'obs' => '3a4',
             'user_id' => 1,
-            'region_id' => 2 // Sob1
+            'region_id' => 2, // Sob1
+            'created_at' => now(),
         ]);
 
         DB::table('freelances')->insert([
             'obs' => 'Ar17 7 2',
             'user_id' => 1,
-            'region_id' => 3 //Sob2
+            'region_id' => 3, //Sob2
+            'created_at' => now(),
         ]);
 
         DB::table('freelances')->insert([
             'obs' => 'L 2',
+            'created_at' => now(),
             'user_id' => 2,
             'region_id' => 1//Vivendas
         ]);
 
         DB::table('freelances')->insert([
             'obs' => '3a4',
+            'created_at' => now(),
             'user_id' => 2,
             'region_id' => 2//Sob1
         ]);
 
         DB::table('freelances')->insert([
             'obs' => 'Ar17 7 2',
+            'created_at' => now(),
             'user_id' => 2,
             'region_id' => 3//Sob2
         ]);
