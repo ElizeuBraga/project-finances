46104320243c2a562d3fff9d1f1c3d7084200558
Elizeu <elizeubragasantos@gmail.com>
2019-07-20 13:25:15 -0300

    RegionSeeder

---
+++ database/seeds/RegionsSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RegionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('regions')->insert([
+            'name' => 'Vivendas Bela Vista',
+            'price' => 0,
+            'user_id' => 1,
+            'rate_id' => 3
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Sobradinho 1',
+            'price' => 0,
+            'user_id' => 1,
+            'rate_id' => 1
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Sobradinho 2',
+            'price' => 0,
+            'user_id' => 1,
+            'rate_id' => 2
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Vivendas Bela Vista',
+            'price' => 0,
+            'user_id' => 2,
+            'rate_id' => 3
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Sobradinho 1',
+            'price' => 0,
+            'user_id' => 2,
+            'rate_id' => 1
+        ]);
+
+        DB::table('regions')->insert([
+            'name' => 'Sobradinho 2',
+            'price' => 0,
+            'user_id' => 2,
+            'rate_id' => 2
+        ]);
+    }
+}
