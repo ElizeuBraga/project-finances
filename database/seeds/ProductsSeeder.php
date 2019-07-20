27acc61e7cb25406e0667e63dd198ab9bf5c9d45
Elizeu <elizeubragasantos@gmail.com>
2019-07-20 13:46:06 -0300

    ProductsSeeder

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -11,9 +11,40 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-        //
-        // DB:table('products')->insert([
-        //     'name' => ''
-        // ])
+        DB::table('products')->insert([
+            'name' => 'Arroz',
+            'user_id' => 1,
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Oleo para motor',
+            'user_id' => 1,
+            'category_id' => 2,
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Aluguel',
+            'user_id' => 1,
+            'category_id' => 3,
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Arroz',
+            'user_id' => 2,
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Oleo para motor',
+            'user_id' => 2,
+            'category_id' => 2,
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Aluguel',
+            'user_id' => 2,
+            'category_id' => 3,
+        ]);
     }
 }
