93500bcd4dc0fb7025fddab8c33a58cafdc4858f
Elizeu <elizeubragasantos@gmail.com>
2019-08-01 13:55:22 -0300

    Add user_id in categories profile view

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/perfil', 'UserController@profile')->name('profile');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/carteira', 'walletController@index')->name('wallet')->middleware('auth');
 Route::post('/carteira', 'walletController@store')->name('wallet')->middleware('auth');
