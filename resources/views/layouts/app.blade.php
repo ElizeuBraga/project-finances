ba3650711dbd2cdcd5a611f7f40203bcccf05099
Elizeu <elizeubragasantos@gmail.com>
2019-08-01 13:31:41 -0300

    Modificando o layoult do meu app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,59 +30,59 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         html, body {
-                background-color: #fff;
-                /* color: #1542E8; */
-                font-family: 'Arial', cursive;
-                font-weight: 500;
-                height: 100vh;
-                margin: 0;
-            }
+            background-color: #fff;
+            /* color: #1542E8; */
+            font-family: 'Arial', cursive;
+            font-weight: 500;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .nav-link {
-                color: red!important;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .nav-link {
+            color: red!important;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+        .m-b-md {
+            margin-bottom: 30px;
+        }
 
-            .logo{
-                width: 50px;
-            }
+        .logo{
+            width: 50px;
+        }
     </style>
 </head>
 <body>
@@ -104,65 +104,82 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('expenses')}}">Gastos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('report')}}">Relatórios</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('product')}}">Produtos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('category')}}">Categorias</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('freelances')}}">Entregas</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                        @if (Request::path() == 'register')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
+                        @if (Request::path() == '/')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Cadastre-se') }}</a>
+                        </li>
+                        @endif
+                        @if (Request::path() == 'login')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @endguest
-                    </ul>
-                </div>
+                        @auth
+                        @if (Request::path() == '/')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/home') }}">{{ __('Home') }}</a>
+                        </li>
+                        @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('expenses')}}">Gastos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('report')}}">Relatórios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('product')}}">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('category')}}">Categorias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('freelances')}}">Entregas</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endif
+                    @endauth
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script>
-        var app = new Vue({
-            el: '#app',
-            data:{
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script>
+    var app = new Vue({
+        el: '#app',
+        data:{
 
-            }
-        })
-    </script>
+        }
+    })
+</script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,119 +1,102 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Righteous&display=swa" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #1542E8;
-                font-family: 'Righteous', cursive;
-                font-weight: 500;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #EB3F25;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            .col-md-4 > a > img {
-                padding: 30px;
-                width: 200px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="">
-                    <img src="imgs/red-pig.svg" alt="" style="display: inline-block; width:400px">
-                </div>
-
-                {{-- <div class="links">
-                    <a href="{{route('wallet')}}">Carteira</a>
-                    <a href="{{route('category')}}">Categorias</a>
-                    <a href="{{route('product')}}">Produtos</a>
-                    <a href="{{route('report')}}">Relatorios</a>
-                </div> --}}
-                <div class="row">
-                    <div class="col-md-4">
-                        <a href="{{route('wallet')}}">
-                            <img src="imgs/wallet.svg" alt="">
-                        </a>
-                    </div>
-                    <div class="col-md-4">
-                        <a href="#">
-                            <img src="imgs/wallet.svg" alt="">
-                        </a>
-                    </div>
-                    <div class="col-md-4">
-                        <a href="#">
-                            <img src="imgs/wallet.svg" alt="">
-                        </a>
-                    </div>
-                </div>
-            </div>
+@extends('layouts.app')
+@section('content')
+<div class="row justify-content-center">
+            <img src="/imgs/red-pig.svg">
         </div>
-    </body>
-</html>
+@endsection
+
+{{-- <!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Righteous&display=swa" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #1542E8;
+            font-family: 'Righteous', cursive;
+            font-weight: 500;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #EB3F25;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        .col-md-4 > a > img {
+            padding: 30px;
+            width: 200px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="/imgs/red-pig.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+                App finances
+            </a>
+            <div class="row">
+                @auth
+                <a class="btn" href="/home">Home</a>
+                @endauth
+
+                @guest
+                <a class="btn" href="/login">Login</a>
+                <a class="btn" href="/register">Cadastrar</a>                    
+                @endguest
+            </div>
+        </nav>
+        <div class="row justify-content-center">
+            <img src="/imgs/red-pig.svg">
+        </div>
+    </div>
+</body>
+</html> --}}
