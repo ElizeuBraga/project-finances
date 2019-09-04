1cdde51b6551167ce5c152c044a2089c9eee44c5
Elizeu <elizeubragasantos@gmail.com>
2019-09-04 19:00:16 -0300

    novo layoult do home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-            <div class="card-header">Seja bem vindo <b>{{Auth::user()->name}}</b>!</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Aqui teremos controle das finanças pessoais e com o passar do tempo teremos noovidades :)
-                </div>
-            </div>
-        </div>
+<div class="row">
+    <div class="col">
+    <center>
+        <a href="/carteira"><img src="/imgs/wallet.png" alt="" style="height: 200px;"></a>
+    </center>
+    </div>
+    <div class="col">
+        <center>
+            <a href="/despesas"><img src="/imgs/expense.png" alt="" style="height: 200px;">
+        </center>
+    </div>
+    <div class="col">
+        <center>
+            <a href="/relatorios"><img src="/imgs/report.png" alt="" style="height: 200px;">
+        </center>
     </div>
 </div>
 @endsection
