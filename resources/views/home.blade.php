11a0daaac9f543cb706d6fcded26116c9b580fc3
Elizeu <elizeubragasantos@gmail.com>
2019-09-05 23:55:08 -0300

    h3 nos icones da tela

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,19 @@
     <div class="col">
     <center>
         <a href="/carteira"><img src="/imgs/wallet.png" alt="" style="height: 200px;"></a>
+        <h3>Carteira</h3>
     </center>
     </div>
     <div class="col">
         <center>
-            <a href="/despesas"><img src="/imgs/expense.png" alt="" style="height: 200px;">
+            <a href="/despesas"><img src="/imgs/expense.png" alt="" style="height: 200px;"></a>
+            <h3>Gastos</h3>
         </center>
     </div>
     <div class="col">
         <center>
-            <a href="/relatorios"><img src="/imgs/report.png" alt="" style="height: 200px;">
+            <a href="/relatorios"><img src="/imgs/report.png" alt="" style="height: 200px;"></a>
+            <h3>Relatótrios</h3>
         </center>
     </div>
 </div>
