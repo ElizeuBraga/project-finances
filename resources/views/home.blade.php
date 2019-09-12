fe113740229bcf2130e0bbd6b8ee65ac0ac272fd
Elizeu <elizeubragasantos@gmail.com>
2019-09-12 00:35:46 -0300

    novo layout em home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,71 @@
 @extends('layouts.app')
 @section('style')
-<style>
-    .icons{
-        padding: 12px 12px 12px 12px;
-        height: 200px;
-        border: solid;
-        border-color: tomato;
-    }
+    <style>
+        @media screen and (min-width: 800px) {
+            .col-md-6{
+                height: 250px;
+                margin-top: 15px;
+                margin-bottom: 2px;
+            }
 
-    .col{
-        padding: 10px;
-        margin-left: 80px;
-    }
-</style>
+            img{
+                height: 100%;
+                border: solid green 1px;
+            }
+
+            a>img{
+                margin-left: 25%;
+            }
+        }
+
+        @media only screen and (max-width: 600px) {
+            .container{
+                width: 96%
+            }
+            .col-md-6{
+                width: 100%;
+                height: 250px;
+                margin-top: 15px;
+                margin-bottom: 2px;
+            }
+            
+            img{
+                height: 100%;
+                width: 100%;
+                border: solid green 1px;
+                border-radius: 5px;
+            }
+        }
+    </style>
 @endsection
 @section('content')
-<div class="row">
-    <div class="col">
-        <a href="/despesas"><img class="icons" src="/imgs/expense.png" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <a href="{{route('expenses')}}">
+                    <img src="images/img-09.svg" alt="">
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="{{route('wallet')}}">
+                    <img src="images/img-10.svg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <a href="{{route('report')}}">
+                    <img src="images/img-11.svg" alt="">
+                </a>
+            </div>
+            <div class="col-md-6">
+            <a href="{{route('category')}}">
+                    <img src="images/img-12.svg" alt="">
+                </a>
+            </div>
+        </div>
     </div>
-    <div class="col">
-        <a href="/carteira"><img class="icons" src="/imgs/wallet.png" alt=""></a>
-    </div>
-    <div class="col">
-        <a href="/relatorios"><img class="icons" src="/imgs/report.png" alt="">
-    </div>
-</div>
+@endsection
+@section('script')
+    
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,207 +1,29 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'App Finances') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    {{-- <script src="{{ asset('js/jquery-3.4.1.min.js') }}" defer></script> --}}
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
-    {{-- <script src="{{ asset('js/inputmask.min.js') }}" defer></script> --}}
-    <script src="js/inputmask.min.js"></script>
-    <script>
-        $(document).ready(function($){
-            $('.dinheiro').mask('#.##0,00', {reverse: true});
-        });
-    </script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>@yield('title')</title>
     <style>
-        html, body {
-            background-color: #fff;
-            /* color: #1542E8; */
-            font-family: 'Arial', cursive;
-            font-weight: 500;
-            height: 100vh;
-            margin: 0;
+        body{
+            background-image: linear-gradient(to right, #43e97b 0%, #38f9d7 100%);
         }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .nav-link {
-            color: red!important;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-
-        .logo{
-            width: 50px;
-        }
-        
-        p{
-            font-size: 18px;
-        }
-
-        .img-article{
-            width: 200px;
-            float: left;
-            margin-right: 20px;
-        }
-
-        .negative{
-            color: red;
-        }
-
-        .positive{
-            color: blue;
+        .container{
+            margin-top: 2%;
+            background: white;
+            border-radius: 10px;
+            margin-bottom: 2%;
         }
     </style>
     @yield('style')
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img class="logo" src="imgs/red-pig.svg" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        @if (Request::path() == 'register')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @endif
-                        @if (Request::path() == '/')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Cadastre-se') }}</a>
-                        </li>
-                        @endif
-                        @if (Request::path() == 'login')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @endguest
-                        @auth
-                        @if (Request::path() == '/')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/home') }}">{{ __('Home') }}</a>
-                        </li>
-                        @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('expenses')}}">Gastos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('wallet')}}">Carteira</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('report')}}">Relatórios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('product')}}">Produtos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('category')}}">Categorias</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="/perfil">Perfil</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    {{ __('Sair') }}
-                                </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                            </div>
-                    </li>
-                    @endif
-                    @endauth
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <main class="container">
+    <div class="container">
+        @include('layouts.nav')
         @yield('content')
-    </main>
-</div>
-@yield('script')
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<script>
-    var app = new Vue({
-        el: '#app',
-        data:{
-
-        }
-    })
-</script>
+        @yield('script')
+    </div>
 </body>
 </html>
