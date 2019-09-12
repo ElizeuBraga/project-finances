98fefd05716e78130b4798cdebe71344dfe20952
Elizeu <elizeubragasantos@gmail.com>
2019-09-12 13:36:17 -0300

    Indentação

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -8,17 +8,17 @@
                 <div class="card-header">Categorias</div>
 
                 <div class="card-body">
-                        <form action="{{route('category.submit')}}" method="POST">
-                                <div class="for-group">
-                                    @csrf
-                                    @method('POST')
-                                    <label class="">Nome da categoria</label>
-                                    <input class="form-control" type="hidden" name="user_id" value="{{Auth::user()->id}}">
-                                    <input class="form-control" type="text" name="name" required>
-                                    <br>
-                                    <button class="form-control btn btn-primary" type="submit">Salvar</button>
-                                </div>
-                            </form>
+                    <form action="{{route('category.submit')}}" method="POST">
+                        <div class="for-group">
+                            @csrf
+                            @method('POST')
+                            <label class="">Nome da categoria</label>
+                            <input class="form-control" type="hidden" name="user_id" value="{{Auth::user()->id}}">
+                            <input class="form-control" type="text" name="name" required>
+                            <br>
+                            <button class="form-control btn btn-primary" type="submit">Salvar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
